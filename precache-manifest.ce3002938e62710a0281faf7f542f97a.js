9df5463ea5e7e123768cb807fb646778d1252eaf
Andras Serfozo <subztep@gmail.com>
2020-07-30 14:47:00 +0100

    Build only :ear_of_rice:

---
+++ precache-manifest.ce3002938e62710a0281faf7f542f97a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a35978af0e836b42f7cf2fe413bfd2d",
+    "url": "/synth/index.html"
+  },
+  {
+    "revision": "c0a05309957e594a7210",
+    "url": "/synth/static/js/2.870674d6.chunk.js"
+  },
+  {
+    "revision": "db30d36cc67fe41fff60f7a6d4e2e2fd",
+    "url": "/synth/static/js/2.870674d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86ec92e78fcd34b925c7",
+    "url": "/synth/static/js/main.a558dd72.chunk.js"
+  },
+  {
+    "revision": "3a7d71a10d37ba61e37a",
+    "url": "/synth/static/js/runtime-main.2ab9fd27.js"
+  }
+]);
--- src/components/header/Save.tsx
+++
@@ -1,7 +0,0 @@
-/** @jsx jsx */
-import { jsx } from "@emotion/core"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-
-const Save = () => <FontAwesomeIcon icon={["fal", "save"]} size="lg" />
-
-export default Save
