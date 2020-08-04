5653e3d4ab07762f5c26e7b7b833062ee5410cc6
Andras Serfozo <subztep@gmail.com>
2020-08-04 02:08:33 +0100

    v0.2.1

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/synth/precache-manifest.e2e138bd392eecb6fc85d6472550ea43.js"
+  "/synth/precache-manifest.3712b003c03fbdc86c35cb7770de3aa6.js"
 );
 
 self.addEventListener('message', (event) => {
